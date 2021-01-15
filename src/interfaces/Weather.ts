ab5eebedba5c096af9fecff3dfffd4f137faf761
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-01-15 20:51:52 -0300

    Create interface Weather

---
+++ src/interfaces/Weather.ts
@@ -0,0 +1,10 @@
+export interface Weather {
+  'date': string,
+  'time': string,
+  'tempMax': string,
+  'tempMin': string,
+  'humidity': string,
+  'main': string,
+  'description': string,
+  'icon': string
+}
